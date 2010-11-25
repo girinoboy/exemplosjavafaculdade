b0b0dc2f6de8f1b917b6cca59e9a2c8734261fb9
rpx.japan <rpx.japan@e9a391f8-4722-a6d7-3ca7-b9323f5e8945>
2010-11-25 04:26:19 +0000

    Fim de noite

--- trunk/ProjetoJava/src/Main.java
+++ trunk/ProjetoJava/src/Main.java
@@ -6,10 +6,8 @@ public class Main{
 
 	public static void main(String[] args) {
 		
-		Agencia sp = new Agencia();
-        sp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        sp.setVisible(true);
-		//new Agencia();
+		new Agencia();
+        
 		
 	}
 
